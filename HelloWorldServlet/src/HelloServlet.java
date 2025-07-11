7062e6df96aaf676290d577c61cf57351cc19326
riyasaha49 <hoimontisaha2002@gmail.com>
2025-07-12 01:59:26 +0600

    Add files via upload

---
+++ HelloWorldServlet/src/HelloServlet.java
@@ -0,0 +1,19 @@
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Hello World from Jakarta Servlet 5.0!</h1>");
+    }
+}
